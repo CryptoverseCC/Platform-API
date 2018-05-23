d2c22d652b787cabf09293cb809fd63be96a1300
Grzegorz Kapkowski <grzegorz.kapkowski@gmail.com>
2018-05-23 21:12:31 +0200

    Keep documentation

--- Apps/Ranking/Workers/Simple/app/src/algorithms/experimental/context_feed.py
+++ Apps/Ranking/Workers/Simple/app/src/algorithms/experimental/context_feed.py
@@ -2,23 +2,34 @@
 Context feed
 ============
 
+Algorithm used by https://userfeeds.github.io/examples/examples/feeds/
+
+Return information feeds for given ERC721 or ERC20 with proper amount
+
 Version: 0.1.0
 
-ERC721 example:
+Example:
 
-`ranking/experimental_context_feed;id=ethereum:0x06012c8cf97bead5deae237070f9587f8e7a266d:587035 <https://api.userfeeds.io/ranking/experimental_context_feed;id=ethereum:0x06012c8cf97bead5deae237070f9587f8e7a266d:587035>`_
+`ranking/experimental_context_feed;context=ethereum:0x06012c8cf97bead5deae237070f9587f8e7a266d:134330 <https://api.userfeeds.io/ranking/experimental_context_feed;context=ethereum:0x06012c8cf97bead5deae237070f9587f8e7a266d:134330>`_
 
-Json claim example:
+Claim used by this algorithm looks like this:
 
 .. code-block:: json
 
     {
-        "claim":{
-            "target": "I love catnip"
-        },
-        "context": "ethereum:0x06012c8cf97bead5deae237070f9587f8e7a266d:587035"
+      "claim": {
+        "target": "I'm supercat! (true story)"
+      },
+      "context": "ethereum:0x06012c8cf97bead5deae237070f9587f8e7a266d:341605"
     }
+
+`context` is identifier of and ERC721 token you own
+`claim.target` is an mes
+sage you want to send
+
+For examples of how to send information to Userfeeds Platform using web-components go to https://components.userfeeds.io/
 """
+
 
 from algorithms.utils import materialize_records
 from algorithms.utils import param
