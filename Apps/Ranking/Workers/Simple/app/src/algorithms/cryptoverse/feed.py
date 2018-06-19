5c5025ed7ddcafa895bf6070e244a7c652d0bb9d
Maciej Górski <maciek.gorski@gmail.com>
2018-06-19 17:35:30 +0200

    Add meesage types to cryptoverse feed.

--- Apps/Ranking/Workers/Simple/app/src/algorithms/cryptoverse/feed.py
+++ Apps/Ranking/Workers/Simple/app/src/algorithms/cryptoverse/feed.py
@@ -10,12 +10,30 @@ Example:
 
 """
 
+import re
 from algorithms.cryptoverse import root
 from algorithms.kuba import replies, reactions
+
+
+tokenPattern = re.compile("[a-z]+:0x[0-9a-f]{40}:\d+")
 
 
 def run(conn_mgr, input, **params):
     result = root.run(conn_mgr, input, **params)
     result = replies.run(conn_mgr, result)
     result = reactions.run(conn_mgr, result)
+    set_types(result["items"])
     return result
+
+
+def set_types(items):
+    for i in items:
+        if tokenPattern.match(i["target"]):
+            i["type"] = "follow"
+        elif i["about"]:
+            if tokenPattern.match(i["about"]):
+                i["type"] = "post_to"
+            else:
+                i["type"] = "post_about"
+        else:
+            i["type"] = "regular"
