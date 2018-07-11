e8b71ab7f4a8e665c94d874a4ac4d64a27212171
Maciej Górski <maciek.gorski@gmail.com>
2018-07-11 17:06:15 +0200

    Add algo showing list of active users.

---
+++ Apps/Ranking/Workers/Simple/app/src/algorithms/experimental/latest_purrers.py
@@ -0,0 +1,25 @@
+"""
+Latest purrers
+==============
+
+Version: 0.1.0
+
+"""
+
+from algorithms.utils import materialize_records, sort_by_created_at
+
+LATEST_PURRERS = """
+MATCH (author)-[:AUTHORED]->(claim:Claim)-[:IN]->(package)
+OPTIONAL MATCH (claim)-[:CONTEXT]->(context)
+WHERE io.userfeeds.erc721.isValidClaim(claim)
+RETURN
+    author.id AS author,
+    context.id AS context,
+    max(package.timestamp) AS created_at
+"""
+
+
+def run(conn_mgr, input, **params):
+    query_result = conn_mgr.run_graph(LATEST_PURRERS, params)
+    mapped_items = materialize_records(query_result)
+    return {"items": sort_by_created_at(mapped_items)}
