0140b0729850dff9d4596db47aa4c3ecf083e525
Maciej Górski <maciek.gorski@gmail.com>
2018-07-12 16:31:02 +0200

    Author balance algo working with cryptoverse's feed, thread and single feed.

--- Apps/Ranking/Workers/Simple/app/src/algorithms/experimental/author_balance.py
+++ Apps/Ranking/Workers/Simple/app/src/algorithms/experimental/author_balance.py
@@ -9,6 +9,7 @@ Adds balance of given author as score.
 """
 
 from algorithms.utils import param, pipeable, filter_debug
+from collections import defaultdict
 
 VALUE_BALANCE = """
 MATCH (t:Transfer { asset: {asset} })-[:RECEIVER]->(receiver)
@@ -25,18 +26,26 @@ RETURN '-' + t.amount as amount, sender.id as address
 @filter_debug
 @param("asset", required=True)
 def run(conn_mgr, input, **params):
-    authors = list(set([item["author"] for item in input["items"]]))
-    params["addresses"] = authors
+    authors = find_all_authors(input["items"])
+    params["addresses"] = list(authors)
     results = conn_mgr.run_graph(VALUE_BALANCE, params)
-    scores = {}
+    scores = defaultdict(int)
     for r in results:
-        if r["address"] not in scores:
-            scores[r["address"]] = int(r["amount"])
-        else:
-            scores[r["address"]] += int(r["amount"])
+        scores[r["address"]] += int(r["amount"])
     for i in input["items"]:
         if i["author"] in scores:
             i["score"] = scores[i["author"]]
     with_score_only = filter(lambda x: "score" in x, input["items"])
     sorted_by_score = sorted(with_score_only, key=lambda x: int(x["score"]), reverse=True)
     return {"items": sorted_by_score}
+
+
+def find_all_authors(items):
+    authors = set()
+    authors.update([i["author"] for i in items])
+    for i in items:
+        if i.get("likes"):
+            authors.update(find_all_authors(i["likes"]))
+        if i.get("replies"):
+            authors.update(find_all_authors(i["replies"]))
+    return authors
