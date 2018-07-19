c0dc8698ba91f374989007f04b41c99b747db842
ghostbuster91 <kghost0@gmail.com>
2018-07-19 15:12:11 +0200

    Restore connection to postgres

--- Apps/Ranking/Workers/Simple/app/src/db/managers.py
+++ Apps/Ranking/Workers/Simple/app/src/db/managers.py
@@ -6,22 +6,33 @@ import os
 import requests
 from neo4j.v1 import GraphDatabase, basic_auth
 from retry import retry
+import psycopg2
 
 DB_GRAPH_USER, DB_GRAPH_PASS = os.environ["NEO4J_AUTH"].split('/')
-RDB_GRAPH = os.environ["RDB_GRAPH"]
+NEO_HOST = os.environ["NEO4J_HOST"]
+POSTGRES_USER, POSTGRES_PASS = os.environ["POSTGRES_AUTH"].split('/')
+POSTGRES_HOST = os.environ["POSTGRES_HOST"]
 
 
 class ConnectionManager:
 
     def __init__(self):
         self.connect_graph()
+        self.connect_sql()
 
     @retry(tries=5, delay=5)
     def connect_graph(self):
         logging.info("Connecting to Neo4j...")
-        self.graph_driver = GraphDatabase.driver("bolt://%s:7687" % RDB_GRAPH,
+        self.graph_driver = GraphDatabase.driver("bolt://%s:7687" % NEO_HOST,
                                                  auth=basic_auth(DB_GRAPH_USER, DB_GRAPH_PASS))
         logging.info("Connected to Neo4j")
+
+    @retry(tries=5, delay=5)
+    def connect_sql(self):
+        logging.info("Connecting to PostgreSQL...")
+        self.sql_conn = psycopg2.connect(host=POSTGRES_HOST, dbname="postgres", user="postgres")
+        logging.info("Connected to PostgreSQL")
+
 
     def get_latest_package(self, family):
         family_url_map = {
