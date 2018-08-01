e77e07a30d4ce87e668fa0c67a492bd0009c85c0
Karol Kowalski <ordon1313@gmail.com>
2018-08-01 14:28:42 +0200

    Add author balance rdb for omise go

---
+++ Apps/Ranking/Workers/Simple/app/src/algorithms/experimental/author_balance_graph.py
@@ -0,0 +1,67 @@
+"""
+Author balance
+==============
+
+Version: 0.1.0
+
+Adds balance of given author as score.
+
+"""
+
+from algorithms.utils import param, pipeable, filter_debug
+from collections import defaultdict
+
+VALUE_BALANCE = """
+MATCH (t:Transfer { asset: {asset} })-[:RECEIVER]->(receiver)
+WHERE receiver.id in {addresses}
+RETURN t.amount as amount, receiver.id as address
+UNION ALL
+MATCH (t:Transfer { asset: {asset} })-[:SENDER]->(sender)
+WHERE sender.id in {addresses}
+RETURN '-' + t.amount as amount, sender.id as address
+"""
+
+
+@pipeable
+@filter_debug
+@param("asset", required=True)
+def run(conn_mgr, input, **params):
+    authors = find_all_authors(input["items"])
+    params["addresses"] = list(authors)
+    results = conn_mgr.run_graph(VALUE_BALANCE, params)
+    scores = defaultdict(int)
+    for r in results:
+        scores[r["address"]] += int(r["amount"])
+    for i in input["items"]:
+        if i["author"] in scores:
+            i["score"] = scores[i["author"]]
+    with_score_only = [x for x in input["items"] if x.get("score", 0) > 0]
+    sorted_by_score = sorted(with_score_only, key=lambda x: x["score"], reverse=True)
+    filter_likes_replies(sorted_by_score, scores)
+    return {"items": sorted_by_score}
+
+
+def find_all_authors(items):
+    authors = set()
+    authors.update([i["author"] for i in items])
+    for i in items:
+        if i.get("likes"):
+            authors.update(find_all_authors(i["likes"]))
+        if i.get("replies"):
+            authors.update(find_all_authors(i["replies"]))
+    return authors
+
+
+def filter_likes_replies(items, scores):
+    for i in items:
+        if i.get("likes"):
+            for like in i["likes"]:
+                if like["author"] in scores:
+                    like["score"] = scores[like["author"]]
+            i["likes"] = [x for x in i["likes"] if x.get("score", 0) > 0]
+        if i.get("replies"):
+            for reply in i["replies"]:
+                if reply["author"] in scores:
+                    reply["score"] = scores[reply["author"]]
+            i["replies"] = [x for x in i["replies"] if x.get("score", 0) > 0]
+            filter_likes_replies(i["replies"], scores)
---
+++ Apps/Ranking/Workers/Simple/app/src/algorithms/experimental/author_balance_rdb.py
@@ -0,0 +1,64 @@
+"""
+Author balance
+==============
+
+Version: 0.1.0
+
+Adds balance of given author as score.
+
+"""
+
+from algorithms.utils import param, pipeable, filter_debug
+from collections import defaultdict
+
+VALUE_BALANCE = """
+SELECT received.address as address, received.amount - COALESCE(send.amount, 0) as amount 
+              FROM (SELECT receiver as address, sum(cast(amount as decimal)) as amount from persistent_transfer where asset = 'ethereum:0xd26114cd6ee289accf82350c8d8487fedb8a0c07' and receiver in (SELECT * FROM UNNEST(%(addresses)s)) group by receiver) as received
+    left outer join (SELECT senders as address, sum(cast(amount as decimal)) as amount from persistent_transfer where asset = 'ethereum:0xd26114cd6ee289accf82350c8d8487fedb8a0c07' and senders in (SELECT * FROM UNNEST(%(addresses)s)) group by senders) as send
+    on received.address = send.address;
+"""
+
+supported_assets = "ethereum:0xd26114cd6ee289accf82350c8d8487fedb8a0c07"
+
+@pipeable
+@filter_debug
+@param("asset", required=True)
+def run(conn_mgr, input, **params):
+    authors = find_all_authors(input["items"])
+    results = conn_mgr.run_rdb(VALUE_BALANCE, {"_asset": params["asset"], "addresses": list(authors)})
+    scores = defaultdict(int)
+    for r in results:
+        scores[r["address"]] += int(r["amount"])
+    for i in input["items"]:
+        if i["author"] in scores:
+            i["score"] = scores[i["author"]]
+    with_score_only = [x for x in input["items"] if x.get("score", 0) > 0]
+    sorted_by_score = sorted(with_score_only, key=lambda x: x["score"], reverse=True)
+    filter_likes_replies(sorted_by_score, scores)
+    return {"items": sorted_by_score}
+
+
+def find_all_authors(items):
+    authors = set()
+    authors.update([i["author"] for i in items])
+    for i in items:
+        if i.get("likes"):
+            authors.update(find_all_authors(i["likes"]))
+        if i.get("replies"):
+            authors.update(find_all_authors(i["replies"]))
+    return authors
+
+
+def filter_likes_replies(items, scores):
+    for i in items:
+        if i.get("likes"):
+            for like in i["likes"]:
+                if like["author"] in scores:
+                    like["score"] = scores[like["author"]]
+            i["likes"] = [x for x in i["likes"] if x.get("score", 0) > 0]
+        if i.get("replies"):
+            for reply in i["replies"]:
+                if reply["author"] in scores:
+                    reply["score"] = scores[reply["author"]]
+            i["replies"] = [x for x in i["replies"] if x.get("score", 0) > 0]
+            filter_likes_replies(i["replies"], scores)
