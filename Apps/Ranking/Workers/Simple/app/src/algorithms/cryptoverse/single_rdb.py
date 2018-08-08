d9113302528b3551e776b5648b7d8d50761bbf91
Karol Kowalski <ordon1313@gmail.com>
2018-08-08 17:43:30 +0200

    Fix single_rdb

--- Apps/Ranking/Workers/Simple/app/src/algorithms/cryptoverse/single_rdb.py
+++ Apps/Ranking/Workers/Simple/app/src/algorithms/cryptoverse/single_rdb.py
@@ -16,17 +16,18 @@ from algorithms.utils import param
 
 MY_EXPRESSIONS_QUERY = """
 SELECT id, target, family, sequence, "timestamp" as created_at, author, context, about, labels
-from persistent_claim where context = %(id)s and is_valid_erc721_context(author,%(_asset)s,%(_amount)s, "timestamp")
+from persistent_claim where context = %(id)s and is_valid_erc721_context_smart(author,%(_asset)s,%(_amount)s, "timestamp")
 """
 EXPRESSIONS_ABOUT_ME_QUERY = """
 SELECT id, target, family, sequence, "timestamp" as created_at, author, context, about, labels
 from persistent_claim where about = %(id)s 
-and is_valid_erc721_context(author,SPLIT_PART(context, ':', 1) || ':' || SPLIT_PART(context, ':', 2),SPLIT_PART(context, ':', 3), "timestamp")
+and is_valid_erc721_context_smart(author,SPLIT_PART(context, ':', 1) || ':' || SPLIT_PART(context, ':', 2),SPLIT_PART(context, ':', 3), "timestamp")
 """
 LIKES_TARGETS = """
-SELECT id, target, family, sequence, "timestamp" as created_at, author, context, about, labels
+SELECT id, target, family, sequence, "timestamp" as created_at, author, 
+CASE WHEN is_valid_erc721_context_smart(author,SPLIT_PART(context, ':', 1) || ':' || SPLIT_PART(context, ':', 2),SPLIT_PART(context, ':', 3), "timestamp") THEN context ELSE null END as context, 
+about, labels
 from persistent_claim where id = ANY(%(ids)s) 
-and is_valid_erc721_context(author,SPLIT_PART(context, ':', 1) || ':' || SPLIT_PART(context, ':', 2),SPLIT_PART(context, ':', 3), "timestamp")
 """
 
 
