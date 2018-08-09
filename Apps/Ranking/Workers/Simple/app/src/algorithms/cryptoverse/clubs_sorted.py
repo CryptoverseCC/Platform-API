43a5fbe058a4a0c06ade810f80380155f51ff1df
Maciej Górski <maciek.gorski@gmail.com>
2018-08-09 11:17:28 +0200

    Add clubs sorted algo.

---
+++ Apps/Ranking/Workers/Simple/app/src/algorithms/cryptoverse/clubs_sorted.py
@@ -0,0 +1,35 @@
+"""
+Cryptoverse Clubs Sorted
+========================
+
+Algorithm used by Cryptoverse
+
+Version: 0.1.0
+
+Example:
+
+`ranking/cryptoverse_clubs_sorted <https://api.userfeeds.io/ranking/cryptoverse_clubs_sorted>`_
+
+"""
+
+import jsonsempai
+with jsonsempai.imports():
+    import mapping
+from algorithms.utils import materialize_records
+import re
+
+CLUBS_QUERY = """
+MATCH (club:Entity)<-[:ABOUT]-(claim)
+WHERE club.id IN {clubs}
+RETURN club.id AS id, count(claim) as score
+ORDER BY score DESC
+"""
+
+
+def run(conn_mgr, input, **params):
+    clubs = [value.network + ":" + value.address for name, value in mapping.__dict__.items() if re.match("^[A-Z_]+$", name)]
+    result = conn_mgr.run_graph(CLUBS_QUERY, {"clubs": clubs})
+    result = materialize_records(result)
+    nonempty = [c["id"] for c in result]
+    result.extend([{"id": c, "score": 0} for c in clubs if c not in nonempty])
+    return {"items": result}
