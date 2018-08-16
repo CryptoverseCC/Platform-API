40f54d5a4adc12796aa3d312a3436623521b2092
Karol Kowalski <ordon1313@gmail.com>
2018-08-16 15:02:45 +0200

    New count add 0 when query do not return result

--- Apps/Ranking/Workers/Simple/app/src/algorithms/cryptoverse/club_feed_new_count.py
+++ Apps/Ranking/Workers/Simple/app/src/algorithms/cryptoverse/club_feed_new_count.py
@@ -17,12 +17,15 @@ RETURN id[0] as club_id, id[1] as version, count(c) as count
 def run(conn_mgr, input, **params):
     ids = [[id, version] for id, version in params["versions"].items()]
     result = conn_mgr.run_graph(ROOT_QUERY, {"ids": ids})
+    result = [map_item(item) for item in result]
+    result_map = {item["club_id"]: {item["version"]: item["count"]} for item in result}
+    result.extend([{"club_id": id, "version": version, "count": 0} for id, version in ids if not result_map.get(id) or not result_map.get(id).get(version)])
     return {
-        "items": [map(item) for item in result],
+        "items": result,
     }
 
 
-def map(item):
+def map_item(item):
     return {
         "club_id": item["club_id"],
         "version": item["version"],
