98e3069b80b670cf3dff6b934cd00cd9759ca525
Maciej Górski <maciek.gorski@gmail.com>
2018-09-04 11:36:08 +0200

    Change algo to return all token balances.

---
+++ Apps/Ranking/Workers/Simple/app/src/algorithms/cryptoverse/balances.py
@@ -0,0 +1,37 @@
+"""
+Unsupported balances
+====================
+"""
+
+from algorithms.utils import param, addressPattern
+from collections import defaultdict
+
+ALL_BALANCES = """
+MATCH (t)-[:RECEIVER]->(receiver:Identity { id: {entity} })
+WHERE t.asset STARTS WITH 'ethereum:'
+RETURN t.amount as amount, t.asset as asset
+UNION ALL
+MATCH (t)-[:SENDER]->(sender:Identity { id: {entity} })
+WHERE t.asset STARTS WITH 'ethereum:'
+RETURN '-' + t.amount as amount, t.asset as asset
+"""
+
+IDENTITY_FROM_CONTEXT = """
+MATCH (identity)<-[:RECEIVER]-(:Transfer { asset: {erc721}, amount: {tokenId} })-[:IN]->(p:Package)
+RETURN identity.id as identity
+ORDER BY p.sequence DESC
+LIMIT 1
+"""
+
+
+@param("entity", required=True)
+def run(conn_mgr, input, **params):
+    if not addressPattern.match(params["entity"]):
+        erc721, token_id = params["entity"].rsplit(":", 1)
+        results = conn_mgr.run_graph(IDENTITY_FROM_CONTEXT, {"erc721": erc721, "tokenId": token_id})
+        params["entity"] = results.single()["identity"]
+    results = conn_mgr.run_graph(ALL_BALANCES, params)
+    balances = defaultdict(int)
+    for r in results:
+        balances[r["asset"]] += int(float(r["amount"]))
+    return {k: v for k, v in balances.items() if v > 0}
