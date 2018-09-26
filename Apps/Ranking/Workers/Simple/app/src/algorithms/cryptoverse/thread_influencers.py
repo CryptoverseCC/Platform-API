57de620e0515cd973eb01c622330d65ce75d6c1f
Karol Kowalski <ordon1313@gmail.com>
2018-09-26 11:44:07 +0200

    Count tokens

--- Apps/Ranking/Workers/Simple/app/src/algorithms/cryptoverse/thread_influencers.py
+++ Apps/Ranking/Workers/Simple/app/src/algorithms/cryptoverse/thread_influencers.py
@@ -23,6 +23,10 @@ RETURN
     replyAuthor.id + collect(likeAuthor.id) + collect(replyReplyAuthor.id) AS authors
 """
 
+COUNT_TOKENS_QUERY = """
+SELECT * from token_count(%(addresses)s);
+"""
+
 golden_ratio = [610, 377, 233, 144, 89, 55, 34, 21, 13, 8, 5, 3, 2, 1]
 
 @param("id", required=True)
@@ -33,7 +37,7 @@ def run(conn_mgr, input, **params):
     authors = set()
     for r in result:
         authors |= r["authors"]
-    author_power = {a: 10 for a in authors}
+    author_power = count_tokens(conn_mgr, authors)
     for r in result:
         r["authors"] = [a for a in r["authors"] if author_power[a]]
     result = [r for r in result if r["authors"]]
@@ -59,3 +63,9 @@ def run(conn_mgr, input, **params):
         for index, a in enumerate(r["authors"]):
             author_ratio[a["id"]] += c * golden_ratio[index] / s2
     return author_ratio
+
+
+def count_tokens(conn_mgr, authors):
+    result = conn_mgr.run_rdb(COUNT_TOKENS_QUERY, {"addresses": list(authors)})
+    return {row["address"]: row["count"] if row["count"] else 0.1 for row in result }
+    # return {row["address"]: row["count"] for row in result }
